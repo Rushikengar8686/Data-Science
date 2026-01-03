2fadd02e9de7e95665441fb4d150a87f84977a18
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-03 15:54:19 +0530

    Slove Practice Questions

---
+++ Python/practices.py
@@ -0,0 +1,24 @@
+# #WAP TO INPUT 2 NUMBER AND PRINT THERE SUM
+numOne = int(input("Enter 1st Number"))
+numTwo = int(input("Enter 2st Number"))
+sum = numOne+numTwo
+print("sum to Twp number :",sum)
+
+# #WAP TO INPUT SIDE OF SQUARE AND PRINT ITS AREA
+side  = numOne*numTwo
+print("Side of square is :", side)
+
+#WAP TO INPUT 2 FLOATING NUMBER AND CALCULATE THERE AVG.
+floatingNo1 = float(input("Enter Floating numbern1:"))
+floatingNo2 = float(input("Enter Floating number2:"))
+avgTwoNumber  = floatingNo1+floatingNo2/2
+print("Print Avg of two floaitng number :",avgTwoNumber)
+
+#WAP TO INPUT 2 IN NUMBER A AND B
+# PRINT TRUE IF A IS GRATER THAN OR EQUAL TO B. IF NOT PRINT FALSE
+if(numOne >= numTwo):
+    print(True)
+else:
+    print(False)
+
+print(True) if numOne >= numTwo else False
