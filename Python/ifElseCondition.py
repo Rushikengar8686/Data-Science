2e52275925c77a60c3d72f44374a63216effaed1
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-03 14:05:11 +0530

    Python Start

---
+++ Python/ifElseCondition.py
@@ -0,0 +1,9 @@
+name = input("Enter Name :")
+gender = input("Enter Gender :")
+
+if(name == "Rushi" and gender == "M"):
+    print("Yes")
+elif (name == "Pooja" and gender == 'F'):
+    print("Yes")
+else:
+    print("Condition not Match")
