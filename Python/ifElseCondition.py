5dc2c50855c5c49d087a724889fad7bb61ff8010
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-03 17:32:59 +0530

    Conditions

--- Python/ifElseCondition.py
+++ Python/ifElseCondition.py
@@ -12,3 +12,27 @@ elif(mark >= 60 and mark < 60):
     print("Grade D")
 else:
     print("Fail") 
+
+
+
+#WAP TO CHECK NUMBER IS ENTER BY USER IS EVEN OR ODD
+if(mark %2 ==0):
+    print("Even")
+else:
+    print("Odd")
+
+
+# WAP TO FIND GREATEST 4 NUMBER ENTER BY THE USER
+num1 = int(input("Enter First Number :"))  #10
+num2 = int(input("Enter Second Number :")) #20
+num3 = int(input("Enter Third Number :"))   #30
+num4 = int(input("Enter four Number :"))   #40
+
+if(num1 > num2 and num1 >num3 and num1 > num4):   # 10 > 20 and 10 > 30
+    print("First number is Largest",num1)
+elif(num2 > num3 and num2 > num4):
+    print("Second number is Largest",num2)
+elif(num3 > num4):
+    print("Third number is largest", num3)
+else:
+    print("Four number is largest",num4)
