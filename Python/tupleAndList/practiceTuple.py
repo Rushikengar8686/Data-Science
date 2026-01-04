95a6d1f3c1e1c27138f25e2740c88bcb296575fc
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-04 14:30:09 +0530

    Tuple practice complete

---
+++ Python/tupleAndList/practiceTuple.py
@@ -0,0 +1,15 @@
+# now how to create tuple in python
+tup   = ()
+#now check Datatype of tuple
+print(type(tup))
+
+
+# WAP to count the number of student with "A" grade in the following tuple
+
+grade = ("C","D","D","A","A","R","E","E","A","B")
+print(grade.count("A"))
+
+# WAP STORE ABOVE VALUE IN LIST AND SORT THEM FORM A TO E
+gradeList = ["C","D","D","A","A","R","E","E","A","B"]
+gradeList.sort()
+print(gradeList)
