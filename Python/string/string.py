971cc86cf26b2521dde5cc27d6b68c04ae99647e
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-04 14:15:23 +0530

    new changes added

---
+++ Python/string/string.py
@@ -0,0 +1,8 @@
+# WAP TO INPUT USER FIRST NAME AND PRINT LENGTH
+str  = input("Enter any string :")
+print(len(str))
+
+
+#WAP to find the occurance $ in the String
+print("Find the occurance :", str.count("$"))
+
