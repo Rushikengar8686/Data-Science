e109e9882c4336706771135707a5fdc39d067a3d
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-05 15:29:30 +0530

    While Loop complete

---
+++ Python/loop/whileLopp.py
@@ -0,0 +1,27 @@
+# While Loop Assignment
+#print number 1 to 100
+# count = 1
+# while count <= 100:
+#     print(count)
+#     count +=1
+
+# Print no 100 to 1
+# count = 100
+# while count >= 1:
+#     print(count)
+#     count -= 1
+
+# Print Multiplication tabel of n number
+# n = int(input("Enter any Value::"))
+# i = 1
+# while i <=10:
+#     print(n*i)
+#     i +=1
+
+# Print the element of the following list using loop
+# 1,4,9,16,25,36,49,64,81,100
+list = [ 1,4,9,16,25,36,49,64,81,100]
+index = 0
+while index < len(list):
+    print(list[index])
+    index +=1
